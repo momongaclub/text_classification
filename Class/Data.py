c2b8729b5be31e3fc6fb50803ef51b74190aeb82
momongaclub <threeforest8@gmail.com>
2020-02-25 19:29:31 +0900

    complete all_function

--- Class/Data.py
+++ Class/Data.py
@@ -4,6 +4,8 @@ import pandas as pd
 import torchtext
 import torch
 
+import torch.nn as nn
+
 import Model
 
 PATH = './.data/ag_news_csv/'
@@ -20,9 +22,9 @@ class Data():
 
     def __init__(self):
         self.dataloader = 0
-        self.text = torchtext.data.Field()
+        self.text = torchtext.data.Field(sequential=True,
+                                         lower=True, include_lengths=True)
         self.label = torchtext.data.Field()
-        self.vocab = torchtext.data.Field()
         self.train_ds = 0
         self.val_ds = 0
         self.test_ds = 0
@@ -38,37 +40,39 @@ class Data():
                 fields=[('Label', self.label), ('Text', self.text)])
 
     def make_vocab(self):
-        self.text.build_vocab(self.train_ds.Text, self.val_ds.Text, self.test_ds.Text,
-                              vectors=torchtext.vocab.GloVe(name='6B', dim=300))
-        print(self.text.vocab.vectors.size())
+        self.text.build_vocab(self.train_ds.Text, self.val_ds.Text,
+                              self.test_ds.Text, vectors = \
+                              torchtext.vocab.GloVe(name = '6B', dim = 300))
+        self.label.build_vocab(self.train_ds.Text)
 
     def make_iter(self):
-        self.train_iter, self.val_iter, self.test_iter = torchtext.data.BucketIterator.splits( 
-            (self.train_ds, self.val_ds, self.test_ds), batch_sizes=(TRAIN_BATCH_SIZE, 
-                                                                     VAL_BATCH_SIZE,
-                                                                     TEST_BATCH_SIZE),
-                                                                     )
-                                                                     #device=-1)
+        self.train_iter, self.val_iter, self.test_iter = torchtext.data.Iterator.splits( 
+            (self.train_ds, self.val_ds, self.test_ds), batch_sizes=(
+                                                                TRAIN_BATCH_SIZE, 
+                                                                VAL_BATCH_SIZE,
+                                                                TEST_BATCH_SIZE),
+                                                                )
 
-    def make_dataloader(self):
-        self.data_loader = torch.utils.data.DataLoader(self.train_ds)
 
-    
 def main():
     data_set = Data()
     data_set.make_dataset()
     data_set.make_vocab()
     data_set.make_iter()
-    data_set.make_dataloader()
-    print(data_set.train_ds[0].Text)
-    print(data_set.train_ds[0].Label)
-    print(data_set.data_loader)
-
 
     v_size = data_set.text.vocab.vectors.size()[0]
     emb_dim = data_set.text.vocab.vectors.size()[1]
-    h_dim = 2
-    model = Model.RNNmodel(emb_dim, h_dim, v_size, data_set.text.vocab.vectors)
+    h_dim = 10
+    model = Model.RNNmodel(emb_dim, h_dim, v_size,
+                           data_set.text.vocab.vectors, 16)
+
+    epochs = 0
+    for batch in iter(data_set.train_iter):
+        x, y = batch.Text[0], batch.Label
+        out_ = model.forward(x)
+        print(epochs, out_)
+        epochs+=1
+
 
 if __name__ == '__main__':
     main()

290a5efbdf046f84ddd53016fef30376cf5fb074
momongaclub <threeforest8@gmail.com>
2020-03-10 16:12:04 +0900

    complete

--- Class/Data.py
+++ Class/Data.py
@@ -12,9 +12,10 @@ import spacy
 
 PATH = '.data/ag_news_csv/'
 # PATH = './Class/.data/ag_news_csv/'
+TRAIN = 'train_sample.csv'
 TRAIN = 'train.csv'
 TEST = 'test.csv'
-VALIDATION = 'validation.csv'
+VALIDATION = 'test.csv'
 FORMAT = 'csv'
 TRAIN_BATCH_SIZE = 128
 VAL_BATCH_SIZE = 256
@@ -31,6 +32,7 @@ class Data():
         # batch_first は [batch, x, x]のように一番最初にbatchの次元を持ってくる
         # self.text = torchtext.data.Field(sequential=True, batch_first=True, tokenize=tokenizer)
         self.text = torchtext.data.Field(sequential=True, batch_first=True, lower=True)
+        self.head = torchtext.data.Field(sequential=True, batch_first=True, lower=True)
         self.label = torchtext.data.Field(batch_first=True)
         self.train_ds = 0
         self.val_ds = 0
@@ -44,12 +46,15 @@ class Data():
             torchtext.data.TabularDataset.splits(
                 path=PATH, train=TRAIN, test=TEST,
                 validation=VALIDATION, format=FORMAT,
-                fields=[('Label', self.label), ('Text', self.text)])
+                fields=[('Label', self.label), ('Head', self.head), ('Text', self.text)])
 
     def make_vocab(self):
         # 3種類の辞書を作成,n vectorsを指定すると事前学習したベクトルを読み込める
         self.text.build_vocab(self.train_ds.Text, self.val_ds.Text,
                               self.test_ds.Text, vectors=
+                              torchtext.vocab.GloVe(name='6B', dim=300))
+        self.head.build_vocab(self.train_ds.Head, self.val_ds.Head,
+                              self.test_ds.Head, vectors=
                               torchtext.vocab.GloVe(name='6B', dim=300))
         self.label.build_vocab(self.train_ds.Label,
                                self.val_ds.Label, self.test_ds.Label)
@@ -73,9 +78,10 @@ class simplernn(nn.Module):
         self.embed = nn.Embedding(vocab_size, embedding_dim)
         self.embed.weight.data.copy_(vocab_vectors)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=batch_first)
-        output_dim = 1
+        output_dim = 6
         self.linear1 = nn.Linear(hidden_dim, output_dim)
-        self.softmax = nn.LogSoftmax(dim=1) # TODO
+        #self.softmax = nn.LogSoftmax(dim=1) # TODO
+        self.softmax = nn.LogSoftmax() # TODO
 
     def forward(self, sentence):
         # lstmの最初の入力に過去の隠れ層はないのでゼロベクトルを代入する
@@ -84,8 +90,9 @@ class simplernn(nn.Module):
         y, hidden = self.lstm(embed)
         y = torch.tanh(y)
         y = self.linear1(y)
-        y = torch.tanh(y)
-        y = self.softmax(y)
+        #y = torch.tanh(y)
+        #y = self.softmax(y)
+        #print(y[0])
         #print("hidden", hidden)
         return y
 
@@ -97,29 +104,29 @@ def main():
 
     vocab_size = data_set.text.vocab.vectors.size()[0]
     embedd_dim = data_set.text.vocab.vectors.size()[1]
+    #print(vars(data_set.label.vocab))
+    # 6zigen 
     hidden_dim = 200
     vocab_vectors = data_set.text.vocab.vectors
 
     rnn = simplernn(embedd_dim, hidden_dim, vocab_size, vocab_vectors)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=0.01, momentum=0.9)
 
     for epoch in range(100):
         data_len = len(data_set.train_iter)
         batch_len = 0
         for batch in iter(data_set.train_iter):
             batch_len = batch_len + 1
-            """
-            print(batch)
-            print(batch.Text)
-            print(batch.Label)
-            """
             target = batch.Label
-            #target = target.squeeze()
+            # torch.eye(クラス数)[対象tensor]でonehotへ
+            target = torch.eye(6, dtype=torch.long)[target] # デフォルトfloatになるのでlong指定
+            target = target.squeeze() #次元変換
+            #print('target', target, 'label', tmp)
+            optimizer.zero_grad()
             output = rnn.forward(batch.Text) 
-            loss_function = nn.CrossEntropyLoss()
             loss = loss_function(output, target)
             loss.backward()
-            optimizer = torch.optim.SGD(rnn.parameters(), lr=1e-2, momentum=0.9)
-            optimizer.zero_grad()
             optimizer.step()
             print('epoch:', epoch, 'batch_len', batch_len, '/', data_len, 'loss:', loss.item())
 
